b766c4fe36a596f5ddee5b8340bd5d8a14e78d4a
tapan0810 <arceus0810@gmail.com>
2026-03-12 22:46:21 +0530

    feat : Implemented PlayerServices and Done with Migration

---
+++ Dotnet_API_24_YARP Reverse Proxy Load Balancer/Data/PlayerDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Dotnet_API_24_YARP_Reverse_Proxy_Load_Balancer.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_API_24_YARP_Reverse_Proxy_Load_Balancer.Data
+{
+    public class PlayerDbContext:DbContext
+    {
+        public PlayerDbContext(DbContextOptions<PlayerDbContext> options) : base(options) { }
+        public DbSet<Player> Players => Set<Player>();
+    }
+}
---
+++ Dotnet_API_24_YARP Reverse Proxy Load Balancer/Service/IPlayerService.cs
@@ -0,0 +1,17 @@
+﻿using Dotnet_API_24_YARP_Reverse_Proxy_Load_Balancer.Entities.Dtos;
+
+namespace Dotnet_API_24_YARP_Reverse_Proxy_Load_Balancer.Service
+{
+    public interface IPlayerService
+    {
+        public Task<List<GetAllPlayerDto>> GetAllPlayers(int pageNumber, int pageSize);
+
+        public Task<GetPlayerByIdDto?> GetPlayerById(int id);
+
+        public Task<GetPlayerByIdDto?> CreatePlayer(CreatePlayerDto dto);
+
+        public Task<bool> UpdatePlayer(int id,UpdatePlayerDto dto);
+
+        public Task<bool> DeletePlayer(int id);
+    }
+}
