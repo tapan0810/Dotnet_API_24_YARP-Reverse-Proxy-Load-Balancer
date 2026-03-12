f85be9b7b064da8c89e59c00a974383cb2c17cf9
tapan0810 <arceus0810@gmail.com>
2026-03-12 23:20:23 +0530

    feat : Implemented Player Controller

---
+++ Dotnet_API_24_YARP Reverse Proxy Load Balancer/Controllers/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using Dotnet_API_24_YARP_Reverse_Proxy_Load_Balancer.Entities.Dtos;
+using Dotnet_API_24_YARP_Reverse_Proxy_Load_Balancer.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_API_24_YARP_Reverse_Proxy_Load_Balancer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController(IPlayerService _service) : ControllerBase
+    {
+        [HttpGet("GetAllPlayers")]
+        public async Task<ActionResult<List<GetAllPlayerDto>>> GetAllPlayer(int pageNumber, int pageSize)
+        {
+            var player = await _service.GetAllPlayers(pageNumber, pageSize);
+
+            if (player == null)
+            {
+                return BadRequest("No Player exists !");
+            }
+
+            return Ok(player);
+        }
+
+        [HttpGet("{id:int}", Name = "GetPlayerById")]
+        public async Task<ActionResult<GetPlayerByIdDto?>> GetPlayerById(int id)
+        {
+            var palyer = await _service.GetPlayerById(id);
+            if (palyer == null)
+            {
+                return BadRequest("No Player exist with this Id");
+            }
+
+            return Ok(palyer);
+        }
+
+        [HttpPost("CreateNewPlayer")]
+        public async Task<ActionResult<GetPlayerByIdDto?>> CreatePlayer(CreatePlayerDto dto)
+        {
+            var player = await _service.CreatePlayer(dto);
+            if (player is null) return BadRequest("Invalid Player Data Entered");
+
+            return CreatedAtAction(nameof(GetPlayerById), new { id = player.PlayerId }, player);
+        }
+
+        [HttpPut("{id:int}", Name = "UpdatePlayerById")]
+        public async Task<ActionResult<bool>> UpdatePlayer(int id, UpdatePlayerDto dto)
+        {
+            var player = await _service.UpdatePlayer(id, dto);
+
+            if (player == false)
+            {
+                return BadRequest("Not able to Update");
+            }
+
+            return Ok(new
+            {
+                message = "Player Udated Successfully",
+                data = player
+            });
+        }
+
+        [HttpDelete("id:int", Name = "DeleteStudentById")]
+        public async Task<ActionResult<bool>> DeleteStudent(int id)
+        {
+            var player = await _service.DeletePlayer(id);
+
+            if (player == false)
+            {
+                return BadRequest("Player with this Id not Found");
+            }
+            return Ok(new
+            {
+                message = "Player Deleted Successfully !",
+                data = player
+            });
+        }
+    }
+}
